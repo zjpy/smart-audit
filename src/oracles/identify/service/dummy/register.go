8035914ecca27e16fea72a76782b30fea5757a6d
Yan Mingzhi <yanmingzhi@cloudwalk.cn>
2020-06-03 14:28:18 +0800

    modify return about golang dummy services

--- src/oracles/identify/service/dummy/register.go
+++ src/oracles/identify/service/dummy/register.go
@@ -3,10 +3,13 @@ package dummy
 import "core/contract"
 
 type EntityIdentifyRegistration struct {
+	dummyID uint32
 }
 
 // 模拟注册物体识别规则
 func (f *EntityIdentifyRegistration) Register(args []string) (contract.ServiceRuleID, error) {
 	// 由于假设是固定的验证规则，所以这里不需要额外工作
-	return 0, nil
+	rtn := contract.ServiceRuleID(f.dummyID)
+	f.dummyID++
+	return rtn, nil
 }
--- src/oracles/location/service/dummy/register.go
+++ src/oracles/location/service/dummy/register.go
@@ -3,10 +3,13 @@ package dummy
 import "core/contract"
 
 type LocationRegistration struct {
+	dummyID uint32
 }
 
 // 模拟注册位置规则
 func (t *LocationRegistration) Register(args []string) (contract.ServiceRuleID, error) {
 	// 由于假设是固定的验证规则，所以这里不需要额外工作
-	return 0, nil
+	rtn := contract.ServiceRuleID(t.dummyID)
+	t.dummyID++
+	return rtn, nil
 }
